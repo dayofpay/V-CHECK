28e202ecb0b8f0a3aa39a8f33dbaf2f46f367030
dayofpay <42325421+dayofpay@users.noreply.github.com>
2022-04-05 20:00:23 +0300

    1.0
    Първа версия

---
+++ VCheck/Modules.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VCheck
+{
+    internal class Modules
+    {
+        public static void CreateWarn(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write($"[ПРЕДУПРЕЖДЕНИЕ] : {message}");
+            Console.WriteLine();
+        }
+        public static void CreateError(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("[ГРЕШКА] : "+ message);
+            Console.WriteLine();
+        }
+        public static void CreateInfo(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("[Информация] : " + message);
+            Console.WriteLine();
+        }
+        public static void CreateAwait(int seconds)
+        {
+            // await
+            System.Threading.Thread.Sleep(seconds * 1000);
+        }
+        public static void ClearLogs()
+        {
+            Console.Clear();
+        }
+    }
+}
---
+++ VCheck/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace VCheck
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            // До тук са познати няколко проблема, в следващата версия ще бъдат оправени ... //
+        Restart:
+            Console.Title = "V-CHECK | НАЧАЛНО МЕНЮ | ДОВРЕ ДОШЪЛ," + Environment.UserName;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"[{DateTime.Now.ToString()}]    Добре дошъл, {Environment.UserName}" !);
+            Console.WriteLine("[1] Проверка на списъка ви със частни номера");
+            Console.WriteLine("[2] Проверка на списъка ви със стандартни номера");
+            Console.WriteLine("[3] Генериране на регистрационни номера");
+            var izbor = int.Parse(Console.ReadLine());
+            try
+            {
+                if (izbor == 1)
+                {
+                    checkLicense.Check("custom");
+                }
+                if (izbor == 2)
+                {
+                    checkLicense.Check("standart");
+                }
+                if (izbor == 3)
+                {
+                    Console.Write("Местоположение за генериране: ");
+                    var placeType = Console.ReadLine();
+                    Console.WriteLine();
+                    Console.Write("Брой рег. номера за генериране: ");
+                    var totalAmount = int.Parse(Console.ReadLine());
+                    Console.WriteLine();
+                    Console.WriteLine("Да съдържа ли списъка поръчкови номера ? (да/не): ");
+                    var uniqueList = Console.ReadLine();
+                    if (uniqueList.ToLower() == "да")
+                    {
+                        CreateVehicleList.CreateVehicles(placeType.ToUpper(), totalAmount, true);
+                    }
+                    else
+                    {
+                        CreateVehicleList.CreateVehicles(placeType.ToUpper(), totalAmount, false);
+                    }
+                }
+            }catch(Exception ex)
+            {
+                Modules.ClearLogs();
+                Modules.CreateError($"Възникна грешка ... информация {ex.Message}");
+                Modules.CreateAwait(2);
+                goto Restart;
+            }
+            }
+    }
+}
